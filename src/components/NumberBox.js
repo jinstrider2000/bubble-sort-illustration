45c3dbee24ec9caf8d65c3955943bf515ad05983
jinstrider2000 <efrainperezjr@live.com>
2019-05-13 10:47:26 -0400

    tried to get animation working async, not smooth enough

--- src/components/NumberBox.js
+++ src/components/NumberBox.js
@@ -2,6 +2,6 @@ import React from 'react';
 
 export default function NumberBox(props) {
   return (
-    <div id={`array-${props.index}`} className={props.classes}>{props.number}</div>
+    <div id={`array-${props.index}`} className="array-container"><div className="array-number">{props.number}</div></div>
   );
 }
--- src/containers/App.js
+++ src/containers/App.js
@@ -1,8 +1,7 @@
 import React, {Component} from 'react';
 import NumberBox from '../components/NumberBox';
 import '../assets/stylesheets/App.css';
-
-// const classNames = require('classnames');
+const classNames = require('classnames');
 
 class App extends Component {
   
@@ -15,11 +14,29 @@ class App extends Component {
     }
   }
 
+  reset = () => {
+    this.setState({
+      array: Array.from({length: 10}, () => {
+        return {number: 1 + Math.floor(Math.random() * Math.floor(100)), selected: false};
+      })
+    })
+  }
+
+  // slideBoxes = (boxLeft, boxRight) => {
+  //   const slideRight = document.getElementById(`array-${boxLeft.index}`);
+  //   const slideLeft = document.getElementById(`array-${boxRight.index}`);
+  //   for (let step = 10000; step > 1; step--) {
+  //     slideRight.style.right = `${Math.floor(boxLeft.end/step)}px`;
+  //     slideLeft.style.left = `${Math.floor(boxRight.end/step)}px`;
+  //   } 
+  // }
+
   bubbleSort = () => {
     for (let i = 0; i < this.state.array.length; i++) {
       setTimeout(() => {
         for (let j = 1; j < this.state.array.length; j++) {
           if (this.state.array[j - 1].number > this.state.array[j].number) {
+            // this.slideBoxes({index: j-1, start: 0, end: -100}, {index: j, start: 0, end: -100})
             this.setState({array: [...this.state.array.slice(0, j-1), this.state.array[j], this.state.array[j-1], ...this.state.array.slice(j+1)]});
           }
         }
@@ -29,16 +46,22 @@ class App extends Component {
   
   renderNumberBoxes = () => {
     return this.state.array.map((num, i) => {
-      return <NumberBox key={i} index={i} number={num.number} classes=""/>
+      return <NumberBox key={i} index={i} number={num.number} classes={classNames("array-container")}/>
     });
   }
 
   render() {
     return (
-      <div id="sort-container">
-        {this.renderNumberBoxes()}
-        <button onClick={this.bubbleSort}> Sort </button>
-      </div>
+      <React.Fragment>
+        <h1>Bubble Sort Illustration</h1>
+        <div id="sort-container">
+          {this.renderNumberBoxes()}
+        </div>
+        <div id="sort-button-container">
+          <button onClick={this.bubbleSort}> Sort </button>
+          <button onClick={this.reset}> Reset </button>
+        </div>
+      </React.Fragment>
     );
   }
 }
